103741542e914526de0493325f69e1e8470ff076
abdullahi ibrahim <ibrahimabdullahi@gmail.com>
2026-05-02 09:42:16 +0100

    Fix config indentation

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'siwesconnect-secret-2025'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     uri = os.environ.get('DATABASE_URL') or 'sqlite:///siwesconnect.db'
-if uri.startswith('postgres://'):
-    uri = uri.replace('postgres://', 'postgresql+psycopg://', 1)
-elif uri.startswith('postgresql://'):
-    uri = uri.replace('postgresql://', 'postgresql+psycopg://', 1)
-SQLALCHEMY_DATABASE_URI = uri
+    if uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql+psycopg://', 1)
+    elif uri.startswith('postgresql://'):
+        uri = uri.replace('postgresql://', 'postgresql+psycopg://', 1)
+    SQLALCHEMY_DATABASE_URI = uri
