d93a98963d79d013512f8f63b70c89bb0e5a4225
abdullahi ibrahim <ibrahimabdullahi@gmail.com>
2026-05-02 08:16:58 +0100

    Migrate to PostgreSQL

--- config.py
+++ config.py
@@ -5,5 +5,9 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'siwesconnect-secret-2025'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///siwesconnect.db'
+    
+    uri = os.environ.get('DATABASE_URL') or 'sqlite:///siwesconnect.db'
+    if uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
